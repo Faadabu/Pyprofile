b00a2a17c25241fba82abf437fc7e64dac9f025f
Fa'ad Abubakar <faadabu@gmail.com>
2019-06-15 08:40:16 +0100

    views and urls created

--- pyprofile/profiles/views.py
+++ pyprofile/profiles/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
+
+
+def index(request):
+    profile = Profile.objects.all()
+    return render(request, 'index.html', {'profile': profile})
+
+
+def home_view(request):
+    return HttpResponse('<h1>home view</h1>')
+
